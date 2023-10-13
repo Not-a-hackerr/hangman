befa03a22aa1dc07959b47a0a6b8bf3bf30f83e5
Nath <nmwasuku@yahoo.co.uk>
2023-10-13 12:37:40 +0100

    Adds encapsulation by adding functions for checking guess and asking for users guess

--- milestone_3.py
+++ milestone_3.py
@@ -4,13 +4,25 @@ guess_word_list = ["Pineapple", "Mango", "Plum", "Banana", "Orange"]
 word_to_be_guessed = rd.choice(guess_word_list).lower()
 print(word_to_be_guessed)
 
-while True:
+
+def ask_for_letter():
     users_guess  = input("Guess a letter: ").lower()
-    if len(users_guess) == 1 and users_guess.isalpha(): 
-        if users_guess in word_to_be_guessed:
-            print(f"Good guess! {users_guess} is in this word")
+    return users_guess
+
+
+def check_guess(guessed_letter):
+    guessed_letter = str(guessed_letter)
+    if len(guessed_letter) == 1 and guessed_letter.isalpha():
+        if guessed_letter in word_to_be_guessed:
+            print(f"Good guess! {guessed_letter} is in this word")
         else:
-            print(f"Unlucky! {users_guess} is not in the word") 
+            print(f"Unlucky! {guessed_letter} is not in the word") 
     else:
         print("Invalid input. Please, enter a single alphabetical character.")
 
+   
+
+
+while True:
+    letter = ask_for_letter()
+    check_guess(letter)
